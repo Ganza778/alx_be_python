d1586ebfc1b718726eefd3c0ede2d72f01405192
Aristide <gnaristide@gmail.com>
2025-07-22 17:56:49 +0200

    I just made a shopping list file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = ["blue","red","green","indigo"]
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add = input("Enter item to add: ")
+            shopping_list.append(add)
+        
+        elif choice == '2':
+            remove = input("Enter item to remove: ")
+            if remove not in shopping_list:
+                print("Invalid option: Item not found in the list.")
+            else:
+             shopping_list.remove(remove)
+        
+        elif choice == '3':
+            for item in shopping_list:
+                print(item)
+          
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
