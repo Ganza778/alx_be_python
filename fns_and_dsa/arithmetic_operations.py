ee4c0401d450c2eea0fc9110e2e2c284852ffeff
Aristide <gnaristide@gmail.com>
2025-07-22 17:07:54 +0200

    updates

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-       return  num1 + num2
-    elif  operation == "subtract":
-       return num1 - num2
-    elif  operation == "multiply":
-       return num1 * num2
-    elif  operation == "divide":
-       if num2==0:
-        return "division by zero is impossible"
-       else:
-         return num1 / num2
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "division by zero is impossible"
+        else:
+            return num1 / num2
     else:
         return "Invalid operator"
-    
