a7f51a20c4986f47851f3f560b3dd6213f72e514
Aristide <gnaristide@gmail.com>
2025-07-24 17:59:02 +0200

    this is a temperature conversation tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
-temperature = float(input("Enter the temperature: "))
+temperature = float(input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 if unit == "f":  
   def convert_to_celsius():
