26df7dd1ddeca9ec3a99a1e7a3b1027371418d7c
Aristide <gnaristide@gmail.com>
2025-08-05 17:46:57 +0200

    i just made a book class

--- condition/tempCodeRunnerFile.py
+++ condition/tempCodeRunnerFile.py
@@ -1,18 +1,6 @@
-import unittest
+import pickle
 
-def add(x, y):
-  """Returns the sum of two numbers."""
-  return x + y
-
-class TestAdd(unittest.TestCase):
-
-  def test_add_positive(self):
-    result = add(5, 3)
-    self.assertEqual(result, 8)
-
-  def test_add_negative(self):
-    result = add(-2, 7)
-    self.assertEqual(result, 5)
-
-if __name__ == "__main__":
-  unittest.main()
+# Deserialize the object from the file
+with open('data.pkl', 'rb') as file:
+    loaded_data = pickle.load(file)
+    print(loaded_data)
